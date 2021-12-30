3123395053dae22a751c3849eae2b7e7293eb62a
alessamashitah <alessa.mashitahzj@gmail.com>
2021-12-30 12:21:13 +0800

    add api guard

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'passport' => 'session',
+            'provider' => 'users',
+            'hash' => False,
+        ],
     ],
 
     /*
