84b1dc065dc134b6ee44398dadb7e3f54627817e
alessamashitah <alessa.mashitahzj@gmail.com>
2022-01-03 10:17:26 +0800

    add api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/fetch/users', [App\Http\Controllers\API\UserAPIController::class, 'index']);
